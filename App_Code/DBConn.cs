198e5324cb5b65c68ec9036b02665ff7afd5d0ef
Sean <seon2you@hotmail.com>
2017-02-21 10:30:57 -0800

    Add project files.

---
+++ App_Code/DBConn.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+/// <summary>
+/// Summary description for DBConn
+/// </summary>
+public class DBConn
+{
+    //1.Database Connection 정보
+    string connectionString = WebConfigurationManager.ConnectionStrings["ASPNET"].ConnectionString;
+    public SqlConnection dbConn;
+
+    //2.생성자를 이용하여 생성과 동시에 데이터 접속
+    public DBConn()
+    {
+        dbConn = new SqlConnection(connectionString);
+        dbConn.Open();
+    }
+
+    //3. DB 연결 닫기
+    public void Close()
+    {
+        dbConn.Close();
+    }
+
+    //4. 연결 객체 반환 메서드
+    public SqlConnection GetConn()
+    {
+        return dbConn;
+    }
+}
---
+++ Left.master.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Left : System.Web.UI.MasterPage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string counter = Application["counter"].ToString();
+        for (int i = 0; i < counter.Length; i++)
+        {
+            Image img = new Image();
+            img.ImageUrl = "images/" + counter[i] + ".bmp";
+            img.Height = 30;
+            img.Width = 30;
+            pnlCounter.Controls.Add(img);
+        }
+    }
+
+    protected void btnSubscribe_Click(object sender, EventArgs e)
+    {
+        // 메세지를 위한 변수
+        string Message = "";
+        string connectionString = WebConfigurationManager.ConnectionStrings["ASPNET"].ConnectionString;
+
+    }
+}
